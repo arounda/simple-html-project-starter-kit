0c04230f3b0306fdf107b3a62d712e082acbd78b
Georgii Ishchenko <ge.ishchenko@gmail.com>
2018-07-31 13:01:28 +0300

    Changelog 1. Modify Babel Env Preset preferences 2. Add Browsersync on top of webpack serve 3. Add Static folder and Copy Webpack Plugin to handle ity

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -10,6 +11,7 @@ module.exports = {
   },
   plugins: [
     new BundleAnalyzerPlugin({analyzerMode: process.env.ANALYZE_BUNDLE ? "server" : "disabled"}),
-    new CleanWebpackPlugin(["dist"])
+    new CleanWebpackPlugin(["dist"]),
+    new CopyWebpackPlugin([{from: path.join(__dirname, "src/static"), to: path.join(__dirname, "dist")}])
   ]
 };
