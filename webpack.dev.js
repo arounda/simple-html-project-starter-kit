526877e9f02bf61b5b88b6adb21750a7b13b4194
Georgii Ishchenko <ge.ishchenko@gmail.com>
2018-07-31 13:01:39 +0300

    Changelog 1. Change indentation to tabs 2. Add Cross Env to handle env variables

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,26 +3,26 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "./serve"),
-    hotUpdateChunkFilename: "../hot/hot-update.js",
-    hotUpdateMainFilename: "../hot/hot-update.json"
-  },
-  devtool: "source-map",
-  plugins: [
-    new CopyWebpackPlugin([{
-      from: path.join(__dirname, "src/static"),
-      to: path.join(__dirname, "serve")
-    }]),
-    new BrowserSyncPlugin({
-      host: "localhost",
-      port: 3000,
-      proxy: "http://localhost:9000/"
-    })
-  ],
-  serve: {
-    port: 9000,
-    content: [path.join(__dirname, "./serve")]
-  }
+	mode: "development",
+	output: {
+		path: path.resolve(__dirname, "./serve"),
+		hotUpdateChunkFilename: "../hot/hot-update.js",
+		hotUpdateMainFilename: "../hot/hot-update.json"
+	},
+	devtool: "source-map",
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: path.join(__dirname, "src/static"),
+			to: path.join(__dirname, "serve")
+		}]),
+		new BrowserSyncPlugin({
+			host: "localhost",
+			port: 3000,
+			proxy: "http://localhost:9000/"
+		})
+	],
+	serve: {
+		port: 9000,
+		content: [path.join(__dirname, "./serve")]
+	}
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,14 +4,14 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "",
-  },
-  plugins: [
-    new BundleAnalyzerPlugin({analyzerMode: process.env.ANALYZE_BUNDLE ? "server" : "disabled"}),
-    new CleanWebpackPlugin(["dist"]),
-    new CopyWebpackPlugin([{from: path.join(__dirname, "src/static"), to: path.join(__dirname, "dist")}])
-  ]
+	mode: "production",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "",
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({analyzerMode: process.env.ANALYZE_BUNDLE ? "server" : "disabled"}),
+		new CleanWebpackPlugin(["dist"]),
+		new CopyWebpackPlugin([{from: path.join(__dirname, "src/static"), to: path.join(__dirname, "dist")}])
+	]
 };
