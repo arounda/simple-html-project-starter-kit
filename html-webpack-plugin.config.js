8ed22266130c74f95c3e4544f1b62f30f4a51229
Georgii Ishchenko <georgii.ishchenko@arounda.agency>
2018-08-13 12:40:29 +0300

    Update dependencies

--- html-webpack-plugin.config.js
+++ html-webpack-plugin.config.js
@@ -4,9 +4,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const templateFiles = fs.readdirSync(__dirname + "/src/templates/pages");
 
 const pluginConfig = templateFiles.map(fullName => {
-	const nameWithoutExnetsion = fullName.replace(/\.[^/.]+$/, "");
+	const nameWithoutExtension = fullName.replace(/\.[^/.]+$/, "");
 	return new HtmlWebpackPlugin({
-		filename: `${nameWithoutExnetsion}.html`,
+		filename: `${nameWithoutExtension}.html`,
 		template: `./templates/pages/${fullName}`
 	});
 });
