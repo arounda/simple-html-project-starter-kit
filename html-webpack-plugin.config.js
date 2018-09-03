53b182cf71cc4101408dcbdb4a61551c394a5154
Georgii Ishchenko <georgii.ishchenko@arounda.agency>
2018-09-03 10:24:46 +0300

    add support for sub-directories for html-webpack-config

--- html-webpack-plugin.config.js
+++ html-webpack-plugin.config.js
@@ -1,14 +1,36 @@
 const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const templateFiles = fs.readdirSync(__dirname + "/src/templates/pages");
+const defaultPath = `${__dirname}/src/templates`;
+const initialFolderName = "pages";
 
-const pluginConfig = templateFiles.map(fullName => {
-	const nameWithoutExtension = fullName.replace(/\.[^/.]+$/, "");
-	return new HtmlWebpackPlugin({
-		filename: `${nameWithoutExtension}.html`,
-		template: `./templates/pages/${fullName}`
+const pluginConfig = [];
+
+const getTemplateConfigs = (folderName = initialFolderName, prevPath = defaultPath) => {
+	const path = `${prevPath}/${folderName}`;
+
+	const templateFiles = fs.readdirSync(path);
+
+	templateFiles.forEach(fullName => {
+		const stats = fs.statSync(`${path}/${fullName}`);
+		if(!stats.isDirectory()) {
+	
+			const nameWithoutExtension = fullName.replace(/\.[^/.]+$/, "");
+			const subfolders = folderName !== initialFolderName ? 
+				path.substr(path.indexOf(`${initialFolderName}`) + initialFolderName.length) + "/" : "";
+				
+			pluginConfig.push(new HtmlWebpackPlugin({
+				filename: `${subfolders}${nameWithoutExtension}.html`,
+				template: `${path}/${fullName}`
+			}));
+
+		}
+		else {
+			getTemplateConfigs(fullName, path);
+		}
 	});
-});
+};
+
+getTemplateConfigs();
 
 module.exports = pluginConfig;
